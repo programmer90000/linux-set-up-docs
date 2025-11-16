51e4aadd02b56fcfff47d8cb4e3fe64297012b82
Abdul Rehman <abdulr568programming@outlook.com>
2025-11-16 10:38:59 +0000

    Set height to default

--- emacs/menu-bar/menu-bar.el
+++ emacs/menu-bar/menu-bar.el
@@ -1,7 +1,7 @@
 (setq header-line-format
     (list
         (propertize (concat "HEADER" (make-string 1000 ? ))
-            'face '(:background "red" :foreground "white" :bold t))))
+            'face '(:background "red" :foreground "white" :bold t :height 1.0))))
 
 (setq-default header-line-format header-line-format)
 
