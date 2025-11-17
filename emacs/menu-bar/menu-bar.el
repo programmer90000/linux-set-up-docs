717657f9c955382bc4840382de2d963b54ffc572
Abdul Rehman <abdulr568programming@outlook.com>
2025-11-17 12:27:46 +0000

    Display second button on menu bar

--- emacs/menu-bar/menu-bar.el
+++ emacs/menu-bar/menu-bar.el
@@ -1,9 +1,10 @@
 (defvar header-line-content " ")
+(defvar header-first-button-text "Menu")
 
 (defun initialize-header-line ()
   (setq header-line-format '(:eval header-line-content)))
 
-;; Create dropdown menu
+;; Create dropdown menu for first button
 (defvar header-dropdown-map
   (let ((map (make-sparse-keymap "Header Dropdown")))
     (define-key map [header-line mouse-1] #'header-dropdown-show)
@@ -16,7 +17,6 @@
          (window (posn-window posn))
          (menu-x 0)
          (menu-y (window-header-line-height window))
-         ;; Create position list for popup-menu
          (position (list (list menu-x menu-y) window)))
     (popup-menu
      '(["Switch to Scratch" (switch-to-buffer "*scratch*")]
@@ -26,12 +26,44 @@
        ["Reload Init" (load-file user-init-file)])
      position)))
 
-;; Set header content with dropdown trigger
+;; Create dropdown menu for second button
+(defvar header-dropdown-map-2
+  (let ((map (make-sparse-keymap "Header Dropdown 2")))
+    (define-key map [header-line mouse-1] #'header-dropdown-show-2)
+    map))
+
+(defun header-dropdown-show-2 (event)
+  "Show dropdown menu at bottom-left of second button."
+  (interactive "e")
+  (let* ((posn (event-start event))
+         (window (posn-window posn))
+         ;; Calculate pixel width of first button text
+         (first-button-width (with-selected-window window
+                               (string-width header-first-button-text)))
+         ;; Convert character width to pixels (approximate)
+         (char-width (frame-char-width))
+         (menu-x (* first-button-width char-width))
+         (menu-y (window-header-line-height window))
+         (position (list (list menu-x menu-y) window)))
+    (popup-menu
+     '(["New Buffer" (switch-to-buffer (generate-new-buffer "untitled"))]
+       ["Save File" (save-buffer)]
+       ["Close Buffer" (kill-buffer)]
+       ["Quit Emacs" (save-buffers-kill-terminal)])
+     position)))
+
+;; Set header content with both dropdown triggers
 (setq header-line-content
-      (propertize " Menu "
-                  'face '(:background "blue" :foreground "white")
-                  'mouse-face '(:background "dark blue")
-                  'help-echo "Click for menu"
-                  'keymap header-dropdown-map))
+      (concat
+       (propertize header-first-button-text
+                   'face '(:background "blue" :foreground "white")
+                   'mouse-face '(:background "dark blue")
+                   'help-echo "Click for menu"
+                   'keymap header-dropdown-map)
+       (propertize "Menu 2"
+                   'face '(:background "green" :foreground "white")
+                   'mouse-face '(:background "dark green")
+                   'help-echo "Click for menu 2"
+                   'keymap header-dropdown-map-2)))
 
 (add-hook 'emacs-startup-hook #'initialize-header-line)
