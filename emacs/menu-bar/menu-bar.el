a3ca2b62f8e1dd391643fda6e2e3c89f73449a95
Abdul Rehman <abdulr568programming@outlook.com>
2025-11-17 12:22:03 +0000

    Display menu at bottom left of button

--- emacs/menu-bar/menu-bar.el
+++ emacs/menu-bar/menu-bar.el
@@ -10,15 +10,21 @@
     map))
 
 (defun header-dropdown-show (event)
-  "Show dropdown menu at mouse position."
+  "Show dropdown menu at bottom-left of button."
   (interactive "e")
-  (popup-menu
-   '(["Switch to Scratch" (switch-to-buffer "*scratch*")]
-     ["Open File" (call-interactively 'find-file)]
-     ["Show Buffer List" (list-buffers)]
-     ["Open Config" (find-file user-init-file)]
-     ["Reload Init" (load-file user-init-file)])
-   (if (listp event) (car (last event)) event)))
+  (let* ((posn (event-start event))
+         (window (posn-window posn))
+         (menu-x 0)
+         (menu-y (window-header-line-height window))
+         ;; Create position list for popup-menu
+         (position (list (list menu-x menu-y) window)))
+    (popup-menu
+     '(["Switch to Scratch" (switch-to-buffer "*scratch*")]
+       ["Open File" (call-interactively 'find-file)]
+       ["Show Buffer List" (list-buffers)]
+       ["Open Config" (find-file user-init-file)]
+       ["Reload Init" (load-file user-init-file)])
+     position)))
 
 ;; Set header content with dropdown trigger
 (setq header-line-content
