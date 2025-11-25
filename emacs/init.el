f3de35325dea4075a985c81f88a084cd5867fc08
Abdul Rehman <abdulr568programming@outlook.com>
2025-11-25 13:23:55 +0000

    Disable middle click drag

--- emacs/init.el
+++ emacs/init.el
@@ -169,6 +169,7 @@
 (define-key global-map [tool-bar mouse-1] 'ignore)
 (with-eval-after-load 'dired (define-key dired-mode-map [mouse-1] 'dired-find-file))
 (with-eval-after-load 'info (define-key Info-mode-map [mouse-1] nil))
+(global-set-key [drag-mouse-2] 'ignore)
 
 ;; =============== Search And Replace ===============
 (defvar search-and-replace-buffer "*search-and-replace*")
