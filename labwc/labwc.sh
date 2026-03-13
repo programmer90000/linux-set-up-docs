17bf34c45f938ebb1f64d5acda92f2595f00223e
Abdul Rehman <abdulr568programming@outlook.com>
2026-03-13 15:28:06 +0000

    Ensure scripts are run with user permissions

--- labwc/labwc.sh
+++ labwc/labwc.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ "$EUID" -eq 0 ]; then
+    echo "Error: This script should not be run as root or with sudo."
+    echo "Please run it as a regular user."
+    exit 1
+fi
+
 echo "Refreshing package list"
 sudo apt update
 
