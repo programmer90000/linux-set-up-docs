e80e9ed7a1b3a9071a0d6793ff3203b5cc430e61
Abdul Rehman <abdulr568programming@outlook.com>
2026-03-16 17:47:19 +0000

    Set desktop wallpaper

--- desktop/install-desktop.sh
+++ desktop/install-desktop.sh
@@ -6,5 +6,13 @@ sudo apt update
 echo "Installing PCManFM-Qt"
 sudo apt install -y pcmanfm-qt
 
-echo "Moving settings.conf file"
+echo "Making directories"
+mkdir -p ~/.config/pcmanfm-qt/default/
+
+echo "Copying desktop wallpaper"
+cp desktop/desktop-wallpaper.png ~/.config/pcmanfm-qt/default/
+
+echo "Copying settings.conf file"
 cp desktop/settings.conf ~/.config/pcmanfm-qt/default/
+
+pcmanfm-qt --desktop &
