676f95c4d578933017d0baded87f592a0d3861fb
Abdul Rehman <abdulr568programming@outlook.com>
2026-03-18 11:49:32 +0000

    Fixed command copying desktop wallpaper

--- desktop/install-desktop.sh
+++ desktop/install-desktop.sh
@@ -10,7 +10,7 @@ echo "Making directories"
 log-command-output.sh ["Making directories"] mkdir -p ~/.config/pcmanfm-qt/default/
 
 echo "Copying desktop wallpaper"
-log-command-output.sh ["Copying desktop wallpaper"] desktop/desktop-wallpaper.png ~/.config/pcmanfm-qt/default/
+log-command-output.sh ["Copying desktop wallpaper"] cp desktop/desktop-wallpaper.png ~/.config/pcmanfm-qt/default/
 
 echo "Copying settings.conf file"
 log-command-output.sh ["Copying settings.conf file"] cp desktop/settings.conf ~/.config/pcmanfm-qt/default/
