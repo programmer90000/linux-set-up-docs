82ca8cbd998ee9a5d83e62e8beafdcd1ab19b19a
Abdul Rehman <abdulr568programming@outlook.com>
2026-03-18 14:28:05 +0000

    Install Brave Browser

---
+++ installing-apps/installing-apps.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+log-command-output.sh ["Installing Brave Browser"] echo "Installing Brave Browser"
+log-command-output.sh ["========================"] echo "========================"
+log-command-output.sh ["Installing Curl"] sudo apt install -y curl
+log-command-output.sh ["Downloading Brave's official GPG key"] sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+log-command-output.sh ["Adding Brave's official repository to your system's software sources list"] sudo curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
+log-command-output.sh ["Refreshing package list"] sudo apt update
+log-command-output.sh ["Installing Brave Browser"] sudo apt install -y brave-browser
