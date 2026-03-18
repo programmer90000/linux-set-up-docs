a66b0dca49dfc83f8a13e4b2d0d618c85c687155
Abdul Rehman <abdulr568programming@outlook.com>
2026-03-18 11:28:28 +0000

    Updated install-desktop.sh to use log-command-output.sh file

--- desktop/install-desktop.sh
+++ desktop/install-desktop.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 echo "Refreshing package list"
-sudo apt update
+log-command-output.sh ["Refreshing package list"] sudo apt update
 
 echo "Installing PCManFM-Qt"
-sudo apt install -y pcmanfm-qt
+log-command-output.sh ["Installing PCManFM-Qt"] sudo apt install -y pcmanfm-qt
 
 echo "Making directories"
-mkdir -p ~/.config/pcmanfm-qt/default/
+log-command-output.sh ["Making directories"] mkdir -p ~/.config/pcmanfm-qt/default/
 
 echo "Copying desktop wallpaper"
-cp desktop/desktop-wallpaper.png ~/.config/pcmanfm-qt/default/
+log-command-output.sh ["Copying desktop wallpaper"] desktop/desktop-wallpaper.png ~/.config/pcmanfm-qt/default/
 
 echo "Copying settings.conf file"
-cp desktop/settings.conf ~/.config/pcmanfm-qt/default/
+log-command-output.sh ["Copying settings.conf file"] cp desktop/settings.conf ~/.config/pcmanfm-qt/default/
 
-pcmanfm-qt --desktop &
+echo "Starting PCManFM-Qt"
+log-command-output.sh ["Starting PCManFM-Qt"] pcmanfm-qt --desktop &
