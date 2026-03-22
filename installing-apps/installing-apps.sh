87aab501c8fc5ac0fc2f5b63fb870591b52f59e6
Abdul Rehman <abdulr568programming@outlook.com>
2026-03-22 16:09:35 +0000

    Install other packages for ClamAV Malware Scanner

--- installing-apps/installing-apps.sh
+++ installing-apps/installing-apps.sh
@@ -28,7 +28,7 @@ log-command-output.sh ["Disable starting Iperf3 on boot"] echo "iperf3 iperf3/st
 log-command-output.sh ["Installing Hardinfo2"] sudo DEBIAN_FRONTEND=noninteractive apt install -y hardinfo2
 log-command-output.sh ["Clear the pre-seeded value stopping debconf screen appearing"] echo "PURGE" | sudo debconf-communicate iperf3
 
-log-command-output.sh ["Installing ClamAV Malware Scanner"] sudo apt -y install clamav
+log-command-output.sh ["Installing ClamAV Malware Scanner"] sudo apt -y install clamav clamtk clamav-daemon
 
 log-command-output.sh ["Installing Extrepo"] sudo apt install -y extrepo
 log-command-output.sh ["Copying config.yaml file"] sudo cp installing-apps/config.yaml /etc/extrepo/config.yaml
