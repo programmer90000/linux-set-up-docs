ee17855cdd8c11edbff156fcbc2bd774b4ceee5d
Abdul Rehman <abdulr568programming@outlook.com>
2026-03-22 14:45:19 +0000

    Remove kdeconnect package installed by Kdenlive

--- installing-apps/installing-apps.sh
+++ installing-apps/installing-apps.sh
@@ -5,6 +5,7 @@ log-command-output.sh ["Refreshing package list"] sudo apt update
 log-command-output.sh ["Installing Neovim"] sudo apt install -y neovim
 
 log-command-output.sh ["Installing Kdenlive Video Editor"] sudo apt install -y kdenlive
+log-command-output.sh ["Removing kdeconnect package installed by Kdenlive"] sudo apt remove -y kdeconnect
 
 log-command-output.sh ["Installing FileZilla"] sudo apt install -y filezilla
 
