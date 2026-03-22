8a984918f8073c2fc3399252dfe88f6b626ad5ff
Abdul Rehman <abdulr568programming@outlook.com>
2026-03-22 13:58:13 +0000

    Install Hardinfo2 correctly

--- installing-apps/installing-apps.sh
+++ installing-apps/installing-apps.sh
@@ -18,7 +18,9 @@ log-command-output.sh ["Installing Qdirstat Disk Space Analyzer"] sudo apt insta
 
 log-command-output.sh ["Installing Thunderbird"] sudo apt install -y thunderbird
 
-log-command-output.sh ["Installing Hardinfo"] sudo apt install -y hardinfo
+log-command-output.sh ["Disable starting Iperf3 on boot"] echo "iperf3 iperf3/start_daemon boolean false" | sudo debconf-set-selections
+log-command-output.sh ["Installing Hardinfo2"] sudo DEBIAN_FRONTEND=noninteractive apt install -y hardinfo2
+log-command-output.sh ["Clear the pre-seeded value stopping debconf screen appearing"] echo "PURGE" | sudo debconf-communicate iperf3
 
 log-command-output.sh ["Installing Brave Browser"] echo "Installing Brave Browser"
 log-command-output.sh ["========================"] echo "========================"
