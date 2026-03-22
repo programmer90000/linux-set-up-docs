df9bc09b847965928762a8cd98aeb157be42b9ff
Abdul Rehman <abdulr568programming@outlook.com>
2026-03-22 18:31:38 +0000

    Install curl

--- installing-apps/installing-apps.sh
+++ installing-apps/installing-apps.sh
@@ -2,6 +2,8 @@
 
 log-command-output.sh ["Refreshing package list"] sudo apt update
 
+log-command-output.sh ["Installing curl"] sudo apt install -y curl
+
 log-command-output.sh ["Installing Neovim"] sudo apt install -y neovim
 
 log-command-output.sh ["Installing Shotcut"] sudo apt install -y shotcut
