eac91a93ba15d7782f9cd89ee268535a27a392de
Abdul Rehman <abdulr568programming@outlook.com>
2026-03-22 16:02:39 +0000

    Install CopyQ

--- installing-apps/installing-apps.sh
+++ installing-apps/installing-apps.sh
@@ -18,6 +18,8 @@ log-command-output.sh ["Installing Thunderbird"] sudo apt install -y thunderbird
 
 log-command-output.sh ["Installing Handbrake"] sudo apt install -y handbrake
 
+log-command-output.sh ["Installing CopyQ"] sudo apt install -y copyq
+
 log-command-output.sh ["Disable starting Iperf3 on boot"] echo "iperf3 iperf3/start_daemon boolean false" | sudo debconf-set-selections
 log-command-output.sh ["Installing Hardinfo2"] sudo DEBIAN_FRONTEND=noninteractive apt install -y hardinfo2
 log-command-output.sh ["Clear the pre-seeded value stopping debconf screen appearing"] echo "PURGE" | sudo debconf-communicate iperf3
