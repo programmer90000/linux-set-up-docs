cf3b8c8dbaf43221c49d8428a232ca3ec224b32f
Abdul Rehman <abdulr568programming@outlook.com>
2026-03-22 13:03:24 +0000

    Install VLC Media Player

--- installing-apps/installing-apps.sh
+++ installing-apps/installing-apps.sh
@@ -10,6 +10,8 @@ log-command-output.sh ["Installing FileZilla"] sudo apt install -y filezilla
 
 log-command-output.sh ["Installing Mousepad Text Editor"] sudo apt install -y mousepad
 
+log-command-output.sh ["Installing VLC Media Player"] sudo apt install -y vlc
+
 log-command-output.sh ["Installing Brave Browser"] echo "Installing Brave Browser"
 log-command-output.sh ["========================"] echo "========================"
 log-command-output.sh ["Installing Curl"] sudo apt install -y curl
