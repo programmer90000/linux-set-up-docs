b59f6a183f9e5c84a868fef77e549bed03809a51
Abdul Rehman <abdulr568programming@outlook.com>
2026-03-22 13:08:10 +0000

    Install Qdirstat Disk Space Analyzer

--- installing-apps/installing-apps.sh
+++ installing-apps/installing-apps.sh
@@ -14,6 +14,8 @@ log-command-output.sh ["Installing VLC Media Player"] sudo apt install -y vlc
 
 log-command-output.sh ["Installing Gimp Image Editor"] sudo apt install -y gimp
 
+log-command-output.sh ["Installing Qdirstat Disk Space Analyzer"] sudo apt install -y qdirstat
+
 log-command-output.sh ["Installing Brave Browser"] echo "Installing Brave Browser"
 log-command-output.sh ["========================"] echo "========================"
 log-command-output.sh ["Installing Curl"] sudo apt install -y curl
