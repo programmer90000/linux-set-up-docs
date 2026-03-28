1a957765970527ebfe200a947487c5b26404364e
programmer90000 <abdulr568programming@outlook.com>
2026-03-28 15:01:23 +0000

    Removed echo commands  from install-desktop.sh

--- desktop/install-desktop.sh
+++ desktop/install-desktop.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 
-echo "Refreshing package list"
 log-command-output.sh ["Refreshing package list"] sudo apt update
-
-echo "Installing PCManFM-Qt"
 log-command-output.sh ["Installing PCManFM-Qt"] sudo apt install -y pcmanfm-qt
-
-echo "Making directories"
 log-command-output.sh ["Making directories"] mkdir -p ~/.config/pcmanfm-qt/default/
-
-echo "Copying desktop wallpaper"
 log-command-output.sh ["Copying desktop wallpaper"] sudo cp desktop/desktop-wallpaper.png /usr/share/pixmaps/
-
-echo "Copying settings.conf file"
 log-command-output.sh ["Copying settings.conf file"] cp desktop/settings.conf ~/.config/pcmanfm-qt/default/
-
-echo "Starting PCManFM-Qt"
 log-command-output.sh ["Starting PCManFM-Qt"] pcmanfm-qt --desktop &
-
-echo "Setting desktop wallpaper"
 log-command-output.sh ["Setting desktop wallpaper"] pcmanfm-qt --set-wallpaper="/usr/share/pixmaps/desktop-wallpaper.png"
