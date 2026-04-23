574e7ff981f4190d25b4a06e6476615eeb427c5e
programmer90000 <abdulr568programming@outlook.com>
2026-04-23 16:13:28 +0100

    sfwbar.h file for dash

---
+++ desktop/dash/src/sfwbar.h
@@ -0,0 +1,18 @@
+#ifndef __SFWBAR_H__
+#define __SFWBAR_H__
+
+#include <glib.h>
+#include <json.h>
+
+#if GLIB_MINOR_VERSION < 68
+#define g_memdup2(x,y) g_memdup(x,y)
+#endif
+
+void signal_subscribe ( void );
+void action_lib_init ( void );
+void hypr_ipc_init ( void );
+void wayfire_ipc_init ( void );
+void scanner_init ( void );
+void expr_init ( void );
+
+#endif
