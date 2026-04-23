2968f395eadefedf4ff7385a207aa6fb7187aa12
programmer90000 <abdulr568programming@outlook.com>
2026-04-23 15:56:50 +0100

    popup.h file for dash

---
+++ desktop/dash/src/gui/popup.h
@@ -0,0 +1,22 @@
+#ifndef __POPUP_H__
+#define __POPUP_H__
+
+G_BEGIN_DECLS
+
+#define POPUP_TYPE_WINDOW popup_window_get_type ()
+G_DECLARE_FINAL_TYPE(PopupWindow, popup_window, POPUP, WINDOW, GtkWindow)
+
+typedef struct _PopupWindow {
+    GtkWindow parent;
+} PopupWindow;
+
+G_END_DECLS
+
+GtkWidget *popup_new ( gchar *name );
+GtkWidget *popup_from_name ( gchar *name );
+void popup_trigger ( GtkWidget *parent, gchar *name, GdkEvent *ev );
+void popup_show ( GtkWidget *parent, GtkWidget *popup, GdkSeat *seat );
+void popup_get_gravity ( GtkWidget *widget, GdkGravity *, GdkGravity * );
+void popup_popdown_autoclose ( void );
+
+#endif
