c94e39b5de631ae7505fd7f5ee5ea0e926e41751
programmer90000 <abdulr568programming@outlook.com>
2026-04-23 15:49:42 +0100

    button.c file for dash

---
+++ desktop/dash/src/gui/button.c
@@ -0,0 +1,28 @@
+#include "gui/button.h"
+#include "gui/scaleimage.h"
+
+G_DEFINE_TYPE_WITH_CODE (Button, button, BASE_WIDGET_TYPE, G_ADD_PRIVATE (Button))
+
+static void button_update_value ( GtkWidget *self ) {
+    ButtonPrivate *priv;
+    
+    g_return_if_fail(IS_BUTTON(self));
+    priv = button_get_instance_private(BUTTON(self));
+    
+    scale_image_set_image(GTK_WIDGET(priv->image), base_widget_get_value(self), NULL);
+}
+
+static void button_class_init ( ButtonClass *kclass ) {
+    BASE_WIDGET_CLASS(kclass)->update_value = button_update_value;
+}
+
+static void button_init ( Button *self ) {
+    ButtonPrivate *priv;
+    
+    priv = button_get_instance_private(BUTTON(self));
+    
+    priv->button = gtk_button_new();
+    priv->image = scale_image_new();
+    gtk_container_add(GTK_CONTAINER(priv->button), priv->image);
+    gtk_container_add(GTK_CONTAINER(self), priv->button);
+}
