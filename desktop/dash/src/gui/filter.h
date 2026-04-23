bd1aec853e048e66f094b589be7274da161fb45c
programmer90000 <abdulr568programming@outlook.com>
2026-04-23 15:52:22 +0100

    filter.h file for dash

---
+++ desktop/dash/src/gui/filter.h
@@ -0,0 +1,17 @@
+#ifndef __FILTER_H__
+#define __FILTER_H__
+
+#include <gtk/gtk.h>
+#include "wintree.h"
+
+enum WindowFilter {
+    FILTER_FLOATING = 1,
+    FILTER_MINIMIZED = 2,
+    FILTER_OUTPUT = 4,
+    FILTER_WORKSPACE = 8,
+};
+
+GType filter_type_get ( void );
+gboolean filter_window_check ( GtkWidget *parent, window_t *win );
+
+#endif
