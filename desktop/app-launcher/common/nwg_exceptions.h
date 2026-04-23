af92d25d2c88d8447c4c2c0058b34cc3e46d3bcc
programmer90000 <abdulr568programming@outlook.com>
2026-04-23 15:31:21 +0100

    nwg_exceptions.h file for app launcher

---
+++ desktop/app-launcher/common/nwg_exceptions.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <stdexcept>
+#include <string>
+#include <string_view>
+
+// wraps strerror into std::string
+std::string error_description(int err);
+
+struct ErrnoException: public std::runtime_error {
+    static inline std::string concat(std::string_view a, std::string b) {
+        b.insert(0, a.data(), a.size());
+        return b;
+    }
+    /* Make sure to use this constructor as follows:
+     *
+     *    int err = errno;
+     *    ErrnoException{ "desc", err }
+     *
+     * and NOT
+     *
+     *    ErrnoException{ "desc", errno }
+     *
+     * because creating `string_view` can potentially change `errno` value,
+     * and the initialization order is not guaranteed */
+    ErrnoException(std::string_view desc, int err):
+        std::runtime_error{ concat(desc, error_description(err)) }
+    {
+        // intentionally left blank
+    }
+    ErrnoException(int err): std::runtime_error{ error_description(err) } {
+        // intentionally left blank
+    }
+};
