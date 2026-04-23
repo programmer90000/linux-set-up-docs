afc0538a64ab0dbb8b406df49da4e8d8d396ebbe
programmer90000 <abdulr568programming@outlook.com>
2026-04-23 16:15:03 +0100

    wayland.h file for dash

---
+++ desktop/dash/src/wayland.h
@@ -0,0 +1,18 @@
+#ifndef __WAYLAND_H__
+#define __WAYLAND_H__
+
+#include <gtk/gtk.h>
+
+typedef struct _wayland_iface {
+    gchar *iface;
+    guint32 global;
+    guint32 version;
+} wayland_iface_t;
+
+void wayland_init ( void );
+gpointer wayland_iface_register ( const gchar *interface, guint32 min_ver, guint32 max_ver, const void *impl );
+void foreign_toplevel_init ( void );
+void cw_init ( void );
+void ew_init ( void );
+
+#endif
