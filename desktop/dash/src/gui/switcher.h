a4066f8e1c80025020293e4b807f75558ada93bb
programmer90000 <abdulr568programming@outlook.com>
2026-04-23 15:58:03 +0100

    switcher.h file for dash

---
+++ desktop/dash/src/gui/switcher.h
@@ -0,0 +1,35 @@
+#ifndef __SWITCHER_H__
+#define __SWITCHER_H__
+
+#include "flowgrid.h"
+
+#define SWITCHER_TYPE            (switcher_get_type())
+#define SWITCHER(obj)            (G_TYPE_CHECK_INSTANCE_CAST((obj), SWITCHER_TYPE, Switcher))
+#define SWITCHER_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST((klass), SWITCHER_TYPE, SwitcherClass))
+#define IS_SWITCHER(obj)         (G_TYPE_CHECK_INSTANCE_TYPE((obj), SWITCHER_TYPE))
+#define IS_SWITCHERCLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE((klass), SWITCHER_TYPE))
+
+typedef struct _Switcher Switcher;
+typedef struct _SwitcherClass SwitcherClass;
+
+struct _Switcher {
+    FlowGrid item;
+};
+
+struct _SwitcherClass {
+    FlowGridClass parent_class;
+};
+
+typedef struct _SwitcherPrivate SwitcherPrivate;
+
+struct _SwitcherPrivate {
+    GtkWidget *switcher;
+    gint filter;
+};
+
+GType switcher_get_type(void);
+
+GtkWidget *switcher_new();
+gboolean switcher_is_focused(gpointer uid);
+
+#endif
