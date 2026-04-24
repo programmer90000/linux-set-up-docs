c63aa611a5d8c7c3db20424d9a7c02cd6f9106d7
programmer90000 <abdulr568programming@outlook.com>
2026-04-24 19:26:52 +0100

    Install login screen

---
+++ login-screen/install-login-screen.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install sddm qml6-module-qtquick-virtualkeyboard qml6-module-qtmultimedia xwayland
+sudo systemctl enable sddm
+sudo systemctl set-default graphical.target
