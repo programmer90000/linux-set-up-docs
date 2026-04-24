0e59809ac90a400723fd418a53684ff18b464d95
programmer90000 <abdulr568programming@outlook.com>
2026-04-24 19:29:31 +0100

    Added -y flag to apt install

--- login-screen/install-login-screen.sh
+++ login-screen/install-login-screen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install sddm qml6-module-qtquick-virtualkeyboard qml6-module-qtmultimedia xwayland
+sudo apt install -y sddm qml6-module-qtquick-virtualkeyboard qml6-module-qtmultimedia xwayland
 sudo systemctl enable sddm
 sudo systemctl set-default graphical.target
 sudo cp login-screen/sddm.conf /etc/sddm.conf
