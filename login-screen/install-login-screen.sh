f100a2add1d09df26f01f6fda273528495f3c4e5
programmer90000 <abdulr568programming@outlook.com>
2026-04-24 19:28:38 +0100

    sddm.conf file

--- login-screen/install-login-screen.sh
+++ login-screen/install-login-screen.sh
@@ -4,3 +4,4 @@ sudo apt update
 sudo apt install sddm qml6-module-qtquick-virtualkeyboard qml6-module-qtmultimedia xwayland
 sudo systemctl enable sddm
 sudo systemctl set-default graphical.target
+sudo cp login-screen/sddm.conf /etc/sddm.conf
