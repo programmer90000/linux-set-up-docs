486b13877c28a9031b8e1c6cb69c81b19a45685c
programmer90000 <abdulr568programming@outlook.com>
2026-04-24 19:36:08 +0100

    Reboot after installing login screen

--- login-screen/install-login-screen.sh
+++ login-screen/install-login-screen.sh
@@ -5,3 +5,4 @@ sudo apt install -y sddm qml6-module-qtquick-virtualkeyboard qml6-module-qtmulti
 sudo systemctl enable sddm
 sudo systemctl set-default graphical.target
 sudo cp login-screen/sddm.conf /etc/sddm.conf
+sudo reboot
