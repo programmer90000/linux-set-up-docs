73d8f415bdf722a9062be6195ad3ccfda05950f8
programmer90000 <abdulr568programming@outlook.com>
2026-04-26 19:42:19 +0100

    Fixed error displaying login screen

--- login-screen/install-login-screen.sh
+++ login-screen/install-login-screen.sh
@@ -11,4 +11,5 @@ sudo cp login-screen/theme.conf $THEME_DIR
 sudo chmod -R 755 "${THEME_DIR}"
 sudo mkdir -p /etc/sddm.conf.d/
 sudo cp login-screen/sddm.conf.d/theme.conf /etc/sddm.conf.d/
+sudo ln -s /usr/bin/sddm-greeter-qt6 /usr/bin/sddm-greeter
 sudo systemctl restart sddm
