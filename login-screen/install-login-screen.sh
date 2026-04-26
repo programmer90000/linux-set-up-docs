eb71990b7440c2a2bce125f828bb397b4b8ab1fe
programmer90000 <abdulr568programming@outlook.com>
2026-04-26 19:55:04 +0100

    Disable virtual keyboard

--- login-screen/install-login-screen.sh
+++ login-screen/install-login-screen.sh
@@ -9,6 +9,7 @@ sudo mkdir -p $THEME_DIR
 sudo cp login-screen/Main.qml $THEME_DIR
 sudo cp login-screen/theme.conf $THEME_DIR
 sudo chmod -R 755 "${THEME_DIR}"
+sudo cp login-screen/sddm.conf /etc/sddm.conf
 sudo mkdir -p /etc/sddm.conf.d/
 sudo cp login-screen/sddm.conf.d/theme.conf /etc/sddm.conf.d/
 sudo ln -s /usr/bin/sddm-greeter-qt6 /usr/bin/sddm-greeter
