054d74efef2310a7947329a174f204881a1091e2
programmer90000 <abdulr568programming@outlook.com>
2026-04-27 18:04:05 +0100

    Ensure login screen script is run as regular user

--- login-screen/install-login-screen.sh
+++ login-screen/install-login-screen.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ "$EUID" -eq 0 ]; then
+    echo "Error: This script should not be run as root or with sudo."
+    echo "Please run it as a regular user."
+    exit 1
+fi
+
 THEME_NAME=login-screen
 THEME_DIR="/usr/share/sddm/themes/${THEME_NAME}"
 
