f8b7fc80e332d0a419592c47999519d0c4ad94ea
programmer90000 <abdulr568programming@outlook.com>
2026-04-29 18:18:11 +0100

    fileformat.lua for lualine

---
+++ apps/config/nvim/lualine/lua/lualine/components/fileformat.lua
@@ -0,0 +1,28 @@
+local M = require("lualine.component"):extend()
+
+-- stylua: ignore
+local symbols = {
+  unix = '', -- e712
+  dos = '', -- e70f
+  mac = '', -- e711
+}
+
+-- Initializer
+function M:init(options)
+    -- Run super()
+    M.super.init(self, options)
+    -- Apply default symbols
+    self.symbols = vim.tbl_extend("keep", self.options.symbols or {}, symbols)
+end
+
+-- Function that runs every time statusline is updated
+function M:update_status()
+    local format = vim.bo.fileformat
+    if self.options.icons_enabled then
+        return self.symbols[format] or format
+    else
+        return format
+    end
+end
+
+return M
