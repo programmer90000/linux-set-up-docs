6a9aaf01545c928ccbc92347472e6b310c528ef7
programmer90000 <abdulr568programming@outlook.com>
2026-04-29 18:15:07 +0100

    function_component.lua for lualine

---
+++ apps/config/nvim/lualine/lua/lualine/components/special/function_component.lua
@@ -0,0 +1,19 @@
+local M = require("lualine.component"):extend()
+
+M.update_status = function(self, is_focused)
+	-- 1st element in options table is the function provided by config
+	local ok, retval
+	ok, retval = pcall(self.options[1], self, is_focused)
+	if not ok then
+		return ""
+	end
+	if type(retval) ~= "string" then
+		ok, retval = pcall(tostring, retval)
+		if not ok then
+			return ""
+		end
+	end
+	return retval
+end
+
+return M
