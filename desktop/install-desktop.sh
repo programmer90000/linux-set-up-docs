695fa6bc9fde2f9ab7bae9730e2f5ddbfb81a664
Abdul Rehman <abdulr568programming@outlook.com>
2026-05-07 15:15:38 +0100

    Removed unused packages

--- desktop/install-desktop.sh
+++ desktop/install-desktop.sh
@@ -9,7 +9,7 @@ mkdir -p ~/Desktop/
 sudo mkdir -p /usr/share/icons/custom-icon-theme/
 sudo mkdir -p /usr/share/icons/custom-icon-theme/{apps,mimetypes,actions,devices,emblems,places,status}/{16,22,24,32,48,64,128,256,512}
 sudo cp desktop/index.theme /usr/share/icons/custom-icon-theme/index.theme
-sudo apt install -y build-essential cmake meson ninja-build pkg-config python3-docutils libgtk-3-dev libgtkmm-3.0-dev libgdk-pixbuf-2.0-dev libgtk-layer-shell-dev libcairo2-dev libglib2.0-dev libjson-c-dev nlohmann-json3-dev libwayland-dev libxkbregistry-dev libpulse-dev libasound2-dev libpipewire-0.3-dev libmpdclient-dev
+sudo apt install -y build-essential cmake meson ninja-build pkg-config libgtk-3-dev libgdk-pixbuf-2.0-dev libcairo2-dev libglib2.0-dev libwayland-dev libgtk-layer-shell-dev libxkbregistry-dev
 cd desktop/app-launcher/
 meson setup builddir -Dbuildtype=release
 ninja -C builddir
