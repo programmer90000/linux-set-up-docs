20a52b7b24d56808239ff6d0961192b9091fb1ea
Abdul Rehman <abdulr568programming@outlook.com>
2026-05-08 16:03:38 +0100

    Add icon to desktop with blue background

--- desktop/icons/icons.c
+++ desktop/icons/icons.c
@@ -1,10 +1,18 @@
 #include <gtk/gtk.h>
 #include <gtk-layer-shell/gtk-layer-shell.h>
 
+static void apply_css_styling(GtkWidget *widget, const char *css) {
+    GtkStyleContext *context = gtk_widget_get_style_context(widget);
+    GtkCssProvider *provider = gtk_css_provider_new();
+    
+    gtk_css_provider_load_from_data(provider, css, -1, NULL);
+    gtk_style_context_add_provider(context, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+    g_object_unref(provider);
+}
+
 static void activate(GtkApplication *app, gpointer user_data) {
-    GtkWidget *window;
-    GtkCssProvider *provider;
-    GdkDisplay *display;
+    GtkWidget *window, *flowbox, *grid, *image, *label;
+    GdkPixbuf *pixbuf;
     
     window = gtk_application_window_new(app);
     
@@ -17,20 +25,66 @@ static void activate(GtkApplication *app, gpointer user_data) {
     gtk_layer_set_anchor(GTK_WINDOW(window), GTK_LAYER_SHELL_EDGE_RIGHT, TRUE);
     gtk_layer_set_anchor(GTK_WINDOW(window), GTK_LAYER_SHELL_EDGE_BOTTOM, TRUE);
     
-    provider = gtk_css_provider_new();
-    gtk_css_provider_load_from_data(provider,
+    flowbox = gtk_flow_box_new();
+    gtk_flow_box_set_max_children_per_line(GTK_FLOW_BOX(flowbox), 4);
+    gtk_flow_box_set_homogeneous(GTK_FLOW_BOX(flowbox), TRUE);
+    gtk_flow_box_set_column_spacing(GTK_FLOW_BOX(flowbox), 20);
+    gtk_flow_box_set_row_spacing(GTK_FLOW_BOX(flowbox), 20);
+    
+    grid = gtk_grid_new();
+    gtk_grid_set_row_spacing(GTK_GRID(grid), 8);
+    
+    pixbuf = gdk_pixbuf_new(GDK_COLORSPACE_RGB, TRUE, 8, 48, 48);
+    gdk_pixbuf_fill(pixbuf, 0x4287f5FF);
+    image = gtk_image_new_from_pixbuf(pixbuf);
+    g_object_unref(pixbuf);
+    
+    label = gtk_label_new("Test App");
+    gtk_label_set_line_wrap(GTK_LABEL(label), TRUE);
+    gtk_label_set_max_width_chars(GTK_LABEL(label), 12);
+    gtk_label_set_xalign(GTK_LABEL(label), 0.5);
+    gtk_label_set_yalign(GTK_LABEL(label), 0.5);
+    
+    gtk_grid_attach(GTK_GRID(grid), image, 0, 0, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), label, 0, 1, 1, 1);
+    
+    gtk_container_add(GTK_CONTAINER(flowbox), grid);
+    gtk_container_add(GTK_CONTAINER(window), flowbox);
+    
+    apply_css_styling(window,
         "window {"
             "background-color: transparent;"
         "}"
-        ".background {"
+        "window.background { background-color: transparent; }"
+    );
+    
+    apply_css_styling(flowbox,
+        "flowbox {"
             "background-color: transparent;"
-        "}",
-        -1, NULL);
+        "}"
+        "flowboxchild {"
+            "background-color: transparent;"
+        "}"
+    );
     
-    display = gdk_display_get_default();
-    gtk_style_context_add_provider_for_screen(gdk_display_get_default_screen(display), GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+    apply_css_styling(label,
+        "label {"
+            "color: white;"
+            "text-shadow: 1px 1px 2px black;"
+            "font-size: 12px;"
+            "font-weight: bold;"
+        "}"
+    );
     
-    g_object_unref(provider);
+    apply_css_styling(grid,
+        "grid {"
+            "background-color: transparent;"
+        "}"
+        "grid:hover {"
+            "background-color: rgba(255, 255, 255, 0.1);"
+            "border-radius: 5px;"
+        "}"
+    );
     
     gtk_widget_show_all(window);
 }
