1d23cd8b735b8efc5997f907747a46791b1dd1ad
Abdul Rehman <abdulr568programming@outlook.com>
2026-05-09 14:07:30 +0100

    Corrected path to desktop files

--- desktop/install-desktop.sh
+++ desktop/install-desktop.sh
@@ -13,12 +13,12 @@ sudo make install
 mkdir -p ~/Desktop/
 cd ../../
 cp desktop/desktop-files/brave-browser.desktop ~/Desktop/
-cp desktop/thunderbird.desktop ~/Desktop/
-cp desktop/alacritty.desktop ~/Desktop/
-cp desktop/shotcut.desktop ~/Desktop/
-cp desktop/vlc.desktop ~/Desktop/
-cp desktop/gimp.desktop ~/Desktop/
-cp desktop/hard-info-2.desktop ~/Desktop/
+cp desktop/desktop-files/thunderbird.desktop ~/Desktop/
+cp desktop/desktop-files/alacritty.desktop ~/Desktop/
+cp desktop/desktop-files/shotcut.desktop ~/Desktop/
+cp desktop/desktop-files/vlc.desktop ~/Desktop/
+cp desktop/desktop-files/gimp.desktop ~/Desktop/
+cp desktop/desktop-files/hard-info-2.desktop ~/Desktop/
 mkdir -p ~/.config/desktop-icons/
 cp desktop/icons/order.conf ~/.config/desktop-icons/
 mkdir -p ~/.config/desktop-files/
