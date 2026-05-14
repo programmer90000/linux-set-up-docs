b26ab02a06264ddd129392e639f4478b35e96c71
Abdul Rehman <abdulr568programming@outlook.com>
2026-05-14 20:42:44 +0100

    Make Firefox profile in correct dir

--- apps/installing-apps.sh
+++ apps/installing-apps.sh
@@ -84,6 +84,8 @@ cp apps/config/hardinfo2/settings.ini ~/.config/hardinfo2/
 sudo apt install firefox-esr
 mkdir -p ~/.config/firefox-esr/personal/
 firefox-esr --CreateProfile "personal $HOME/.config/firefox-esr/personal/"
+cp apps/config/firefox/profiles.ini ~/.mozilla/firefox/
+cp apps/config/firefox/profiles.ini ~/.mozilla/firefox-esr/
 echo "Installing Extrepo"
 sudo apt install -y extrepo
 sudo cp apps/config.yaml /etc/extrepo/config.yaml
