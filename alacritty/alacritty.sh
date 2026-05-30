0991e836ca5d8b9d922b081215f8a07f5304aa38
Abdul Rehman <abdulr568programming@outlook.com>
2026-05-30 11:39:18 +0100

    Removed line copying .zsh-aliases file

--- alacritty/alacritty.sh
+++ alacritty/alacritty.sh
@@ -22,6 +22,5 @@ sudo apt install -y tmux
 sudo apt install -y fonts-hack
 sudo apt install -y zsh
 cp alacritty/zsh/.zshrc ~/.config/zsh/
-cp alacritty/zsh/.zsh-aliases ~/.config/zsh/
 ln -s ~/.config/zsh/.zshrc ~/.zshrc
 cp $ALACRITTY_CONFIG ~/.config/alacritty/alacritty.toml
