476f65ffc02adf541360ed8dc92759cf03138ec6
Abdul Rehman <127865680+programmer90000@users.noreply.github.com>
2026-08-15 13:00:33 +0100

    Install btop

--- apps/installing-apps.sh
+++ apps/installing-apps.sh
@@ -130,6 +130,8 @@ echo "Installing QEMU/KVM"
 sudo apt install qemu-kvm libvirt-daemon-system libvirt-clients virtinst bridge-utils
 echo "Installing ssh"
 sudo apt install ssh
+echo "Installing btop"
+sudo apt install btop
 echo "Installing Extrepo"
 sudo apt install -y extrepo
 sudo cp apps/config.yaml /etc/extrepo/config.yaml
