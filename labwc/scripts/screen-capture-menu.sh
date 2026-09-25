5521c178407f924aceb3ee0f2f659fe0f7e513b2
Abdul Rehman <127865680+programmer90000@users.noreply.github.com>
2026-09-25 09:10:23 +0100

    Script for screenshot options

---
+++ labwc/scripts/screen-capture-menu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eu
+
+CHOICE=$(printf '%s\n' "Full Screen" "Select Region" | wofi --dmenu --prompt "Screenshot:" --define=hide_search=true --columns 2 --location top --width 200 --height 33 --insensitive --cache-file /dev/null) || exit 0
+
+case "$CHOICE" in
+    "Full Screen")  exec "$HOME/screenshot.sh" ;;
+    "Select Region") exec "$HOME/screenshot.sh" ;;
+esac
